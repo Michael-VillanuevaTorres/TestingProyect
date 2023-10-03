f36a6ded2a873698959cd8eade2721e865655ee8
MIchael Ignacio Villanueva Torres <mvillanueva2019@inf.udec.cl>
2023-10-03 00:02:17 -0300

    refactor(tests): Refactor product_controller and add test functions
    - Refactor product_controller.py to enable unit testing.
    - Added 3 functions in __init__.py:
      - generate_fake_data() for generating data instances for testing.
      - reset_fake_data() to remove fake data.
      - ver_datos() to check the operation of the functions.
    - Modified test_encargado.py to improve testing logic.
    - Created test_product.py for product-related tests.
    
    This commit includes refactoring product_controller.py to facilitate unit testing and adding test functions for product-related testing. Additionally, three functions have been added to __init__.py for managing fake data instances, and test_encargado.py has been modified for improved testing logic. test_product.py has also been created for product tests.

--- api/website/__init__.py
+++ api/website/__init__.py
@@ -3,6 +3,44 @@ from flask import Flask, abort, render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from faker import Faker
+import random
+
+desarrolladores_fake = [
+    "Desarrollador 1",
+    "Desarrollador 2",
+]
+desarrolladores_fake2 = [
+    "Desarrollador 3",
+    "Desarrollador 4",
+]
+
+# Generar clientes
+clientes_fake = [
+    "Cliente 1",
+    "Cliente 2",
+    "Cliente 3",
+]
+
+# Generar productos
+productos_fake = [
+    "Producto 1",
+    "Producto 2",
+    "Producto 3",
+    "Producto 4",
+]
+emailcliente_fake = [
+    "email1@example.com",
+    "email2@example.com", 
+    "email3@example.com"
+]
+emaildesarrollador_fake =[
+    "desarrollador1@example.com"
+    "desarrollador2@example.com",
+]
+emaildesarrollador_fake2 =[
+    "desarrollador3@example.com",
+    "desarrollador4@example.com"
+]
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -51,11 +89,36 @@ def create_app():
             return render_template('reporte.html', title_reporte=title_reporte, description_reporte=description_reporte)
         except:
             abort(404)
-    @app.route('/ver_datos/productos')
-    def ver_productos():
+
+    @app.route('/ver_datos')
+    def ver_datos():
         productos = Producto.query.all()
-        data = [{'nombre': producto.nombre} for producto in productos]
-        return (data)
+        reportes = Reporte.query.all()
+        clientes = Cliente.query.all()
+        desarolladores = Desarrollador.query.all()
+
+        data = [{'nombre': producto.nombre,'id':producto.id, 'enc': producto. id_encargado} for producto in productos]
+        data2 = [{'nombreR': reporte.titulo,'idR':reporte.id, 'idDeveloper':reporte.id_developer} for reporte in reportes]
+        data3 = [{'nombreC': cliente.nombre,'idC':cliente.id} for cliente in clientes]
+        data4 = [{'nombreP': desarollador.nombre,'idP':desarollador.id} for desarollador in desarolladores]
+                
+        return (data+data2+data3+data4)
+    
+
+    # Ruta para eliminar los datos generados por Fake
+    @app.route('/reset_fake_data') 
+    def reset_fake_data():
+        # Eliminar todos los objetos generados por Fake de la base de datos
+        db.session.query(Desarrollador).delete()
+        db.session.query(Cliente).delete()
+        db.session.query(Producto).delete()
+        db.session.query(Reporte).delete()
+
+        # Cometer los cambios en la base de datos
+        db.session.commit()
+
+        return ({'message': 'Datos generados por Fake eliminados correctamente'}), 200
+
 
     @app.route('/producto/<int:id>')
     def producto(id):
@@ -72,63 +135,65 @@ def create_app():
 
     @app.route('/generate_fake_data')
     def generate_fake_data():
-        from faker import Faker  # Asegúrate de importar Faker
-        fake = Faker()  # Crea una instancia de Faker
-
-        # Crear roles (0 y 1)
+      # Crear roles (0 y 1)
 
         # Crear desarrolladores (3 en total, 2 con rol 0 y 1 con rol 1)
         desarrolladores = []
-        for _ in range(2):
+        for i in range(2):
             desarrollador = Desarrollador(
-                nombre=fake.name(),
-                email=fake.email(),
+                nombre=desarrolladores_fake[i-1],
+                email=emaildesarrollador_fake[i-1],
                 id_rol=0
             )
             desarrolladores.append(desarrollador)
 
-        for _ in range(1):
-            desarrollador = Desarrollador(
-                nombre=fake.name(),
-                email=fake.email(),
+        for i in range(2):
+            desarrollador2 = Desarrollador(
+                nombre=desarrolladores_fake2[i],
+                email=emaildesarrollador_fake2[i],
                 id_rol=1
             )
-            desarrolladores.append(desarrollador)
+            desarrolladores.append(desarrollador2)
 
         # Crear clientes (2 en total)
         clientes = []
-        for _ in range(2):
+        for i in range(3):
             cliente = Cliente(
-                nombre=fake.company(),
-                email=fake.email()
+                nombre=clientes_fake[i],
+                email=emailcliente_fake[i]
             )
             clientes.append(cliente)
 
         # Crear productos (4 en total)
         productos = []
-        for _ in range(4):
+        for i in range(4):
             producto = Producto(
-                nombre=fake.word(),
+                nombre=productos_fake[i],
             )
             productos.append(producto)
+        
+
 
         db.session.add_all(desarrolladores + clientes + productos)
-
+       
         # Crear reportes (2 por cada producto)
         for producto in productos:
             for _ in range(2):
                 reporte = Reporte(
-                    titulo=fake.sentence(),
-                    descripcion=fake.paragraph(),
+                    titulo=f"Reporte de {producto.nombre}",
+                    descripcion=f"Descripción del reporte de {producto.nombre}",
                     id_producto=producto.id,
-                    id_cliente=clientes[fake.random_int(0, 1)].id,  # Escoge un cliente aleatorio
+                    id_cliente=1, 
                 )
                 db.session.add(reporte)
+                Reporte.add_developer(reporte, 1)
+                
 
         # Agregar todos los objetos a la sesión y guardar en la base de datos
         db.session.commit()
         return "Funcionó correctamente"
         
+    
     return app
 
 def create_database(app):
--- api/website/tests/test_encargado.py
+++ api/website/tests/test_encargado.py
@@ -1,127 +1,113 @@
 import pytest
-from controllers.refactor_encargado_controller import *
-from app import app as flask_app
-from unittest.mock import MagicMock
+from website import create_app
+import json
 
-# Fixtures para configuración común
+# Fixtures for common configuration
 @pytest.fixture
 def app_client():
-    return flask_app.test_client()
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.app_context():
+        app.test_client().get('/generate_fake_data')
+    client = app.test_client()
+    yield client
 
-@pytest.fixture
-def mock_database():
-    return MagicMock()
 
-
-def test_add_valid_reasignation_petition(self, app_client):
+def test_add_valid_reasignation_petition(client):
     """
-    GIVEN a request with valid data
-    WHEN the request is sent to add a reasignation petition
-    THEN check it returns a 201 status code
+    Given: The request with valid data.
+    When: The request is sent to add a reassignment request.
+    Then: The response is verified to have a 201 status code.
     """
     response = app_client.post('/reasignacion/add/?id_report=1&id_developer=2', json={'motivo': 'Prueba'})
     assert response.status_code == 201
 
-def test_add_invalid_reasignation_petition(self, app_client):
+def test_add_invalid_reasignation_petition():
     """
-    GIVEN a request with invalid data
-    WHEN the request is sent to add a reasignation petition
-    THEN check it returns a 400 status code
+    Given: The request with invalid data.
+    When: The request is sent to add a reassignment request.
+    Then: The response is verified to have status code 400.
     """
     response = app_client.post('/reasignacion/add/?id_report=1&id_developer=2', json={})
     assert response.status_code == 400
 
-def test_add_valid_solicitud_reasignacion(self):
+def test_delete_valid_solicitud_reasignacion(app_client):
     """
-    GIVEN valid data
-    WHEN add_solicitud_reasignacion is called
-    THEN check it returns a successful response
+    Given: A valid reassignment request in the database.
+    When: A request for deletion is sent.
+    Then: The response is checked for a 201 status code.
     """
-    result = add_solicitud_reasignacion(1, 2, 'Prueba')
-    assert result['success'] is True
-
-def test_add_invalid_solicitud_reasignacion(self):
-    """
-    GIVEN invalid data
-    WHEN add_solicitud_reasignacion is called
-    THEN check it returns a failed response
-    """
-    result = add_solicitud_reasignacion(1, 2, '')
-    assert result['success'] is False
-
-def test_delete_valid_solicitud_reasignacion(self, app_client):
-    """
-    GIVEN a valid reasignation petition in the database
-    WHEN a request is sent to delete it
-    THEN check it returns a 201 status code
-    """
-    add_test_solicitud_reasignacion()
+    
     response = app_client.delete('/reasignacion/delete/?id_report=1&id_dev=1')
     assert response.status_code == 201
 
-def test_delete_non_existing_solicitud_reasignacion(self, app_client):
+def test_delete_non_existing_solicitud_reasignacion(app_client):
     """
-    GIVEN a request to delete a non-existing reasignation petition
-    WHEN the request is sent
-    THEN check it returns a 404 status code
+    Given: A request to delete a reassignment request that does not exist.
+    When: The request is sent.
+    Then: The response is checked for a 404 status code.
     """
     response = app_client.delete('/reasignacion/delete/?id_report=1&id_dev=1')
     assert response.status_code == 404
 
-def test_delete_solicitud_reasignacion_missing_parameters(self, app_client):
+def test_delete_solicitud_reasignacion_missing_parameters( app_client):
     """
-    GIVEN a request to delete a reasignation petition without all required parameters
-    WHEN the request is sent
-    THEN check it returns a 400 status code
+    Given: a request to delete a reasignation petition without all required parameters
+    When: the request is sent
+    Then: check it returns a 400 status code
     """
     response = app_client.delete('/reasignacion/delete/?id_report=1')
     assert response.status_code == 400
 
-def test_delete_solicitud_reasignacion_db_error(self, app_client):
+def test_delete_solicitud_reasignacion_db_error( app_client):
     """
-    GIVEN an environment where a deletion error occurs
-    WHEN a request to delete a valid petition is sent
-    THEN check it returns a 500 status code or an appropriate error code
+    Given: An environment where a delete error occurs.
+    When: A request to delete a valid request is sent.
+    Then: The response is checked for a status code 500 or an appropriate error code.
     """
-    mock_delete_solicitud_error()
-    response = app_client.delete('/reasignacion/delete/?id_report=1&id_dev=1')
+
+    response = app_client.delete('/reasignacion/delete/?id_report=1&id_dev=100')
     assert response.status_code == 500
 
-def test_get_all_reasignation_petitions_from_a_specific_product(self, app_client):
+def test_get_all_reasignation_petitions_from_a_specific_product(app_client):
     """
-    GIVEN a request with a valid 'id_product' parameter
-    WHEN get_all_reasignation_petitions_from_a_specific_product is called
-    THEN check it returns a 200 status code and the expected format
+    Given: A request with a valid 'id_product' parameter.
+    When: The function get_all_reasignation_requests_from_a_specific_product is called.
+    Then: The response is checked for a status code 200 and an expected format.
     """
     response = app_client.get('/reasignacion/get/all/product/?id_product=1')
     assert response.status_code == 200
     assert 'id_report' in response.json[0]
     assert 'id_developer' in response.json[0]
 
-def test_get_motivo_reasignation_petition_from_a_specific_report(self, app_client):
+def test_get_motivo_reasignation_petition_from_a_specific_report( app_client):
     """
-    GIVEN a request with a valid 'id_report' parameter
-    WHEN get_motivo_reasignation_petition_from_a_specific_report is called
-    THEN check it returns a 200 status code and the expected format
+    Given: A request with a valid 'id_report' parameter.
+    When: The get_get_motivo_reasignation_petition_request_from_a_specific_report function is called.
+    Then: The response is verified to have a status code 200 and an expected format.
     """
     response = app_client.get('/reasignacion/get/motivo/report/?id_report=1')
     assert response.status_code == 200
     assert 'motivo' in response.json
 
-def test_get_motivo_reasignation_petition_report_not_found(self, app_client):
+def test_get_motivo_reasignation_petition_report_not_found(app_client):
     """
-    GIVEN a request with a non-existing 'id_report' parameter
-    WHEN get_motivo_reasignation_petition_from_a_specific_report is called
-    THEN check it returns a 400 status code
+    Given: A request with an 'id_report' that does not exist.
+    When: The function get_motif_reasignation_request_request_from_a_specific_report is called.
+    Then: The response is checked for a status code 400.
     """
     response = app_client.get('/reasignacion/get/motivo/report/?id_report=999')
     assert response.status_code == 400
 
-def test_get_motivo_reasignation_petition_report_not_in_solicitud_reasignacion(self, app_client):
+def test_get_motivo_reasignation_petition_report_not_in_solicitud_reasignacion( app_client):
     """
-    GIVEN a request with an 'id_report' not found in the solicitud_reasignacion table
-    WHEN get_motivo_reasignation_petition_from_a_specific_report is called
-    THEN check it returns a 400 status code
+    Given: A request with an 'id_report' that is not found in the request_reassignment_request table.
+    When: The get_reason_reasignation_request_request_from_a_specific_report function is called.
+    Then: Check that the response has status code 400.
     """
     response = app_client.get('/reasignacion/get/motivo/report/?id_report=2')
     assert response.status_code == 400
+
+
+
+
