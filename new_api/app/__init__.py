499fe0933d1db564e5c98a98da85933d67995c0d
MIchael Ignacio Villanueva Torres <mvillanueva2019@inf.udec.cl>
2023-11-05 20:42:34 -0300

    Update __init__.py

--- new_api/app/__init__.py
+++ new_api/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 from config import Config
 from app.extensions import db
+from flask_cors import CORS
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app)
 
     # Initialize Flask extensions here
 
